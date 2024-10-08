ad78ddfa6c0a9fa9773413bba0d88aa2d4fabdf5
nandorocker <talk@nan.do>
2024-10-08 18:22:16 +0200

    Basic working version relying on venv

---
+++ README.md
@@ -0,0 +1,14 @@
+# Like This for Spotify
+
+## Description
+
+A simple small script to like the currently playing song on Spotify.
+
+## Installation
+After installing, using the terminal run:
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+````
---
+++ like_this_track.py
@@ -0,0 +1,25 @@
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Load credentials from config.json
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+# Scope needed to modify the "Liked Songs" playlist
+SCOPE = 'user-library-modify user-read-playback-state'
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config['CLIENT_ID'],
+                                               client_secret=config['CLIENT_SECRET'],
+                                               redirect_uri=config['REDIRECT_URI'],
+                                               scope='user-library-modify user-read-playback-state'))
+
+# Get the current playback information
+current_track = sp.current_playback()
+
+if current_track and current_track['is_playing']:
+    track_id = current_track['item']['id']
+    sp.current_user_saved_tracks_add([track_id])
+    print(f"Added {current_track['item']['name']} by {current_track['item']['artists'][0]['name']} to Liked Songs.")
+else:
+    print("No track is currently playing.")
